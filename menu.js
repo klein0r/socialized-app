04aa4926d87d10d8d80e21651f73b553335e4ec7
Matthias Kleine <matthias@klein0r.de>
2015-09-18 13:33:37 +0200

    Reformatted js

--- menu.js
+++ menu.js
@@ -1,44 +1,54 @@
 
 /*	Define Click Event for Mobile */
-if( 'ontouchstart' in window ){ var click = 'touchstart'; }
-else { var click = 'click'; }
-
-/*	Reveal Menu */
-$('div.button').on(click, function(){
-    if( !$('div.content').hasClass('inactive') ){
-        // Slide and scale content
-        $('div.content').addClass('inactive');
-        setTimeout(function(){ $('div.content').addClass('flag'); }, 100);
-        
-        // Slide in menu links
-        var timer = 0;
-        $.each($('li'), function(i,v){
-            timer = 40 * i;
-            setTimeout(function(){
-                $(v).addClass('visible');
-            }, timer);
-        });
-    }
-});
-
-/*	Close Menu */
-function closeMenu() {		
-    // Slide and scale content
-    $('div.content').removeClass('inactive flag');
-
-    // Reset menu
-    setTimeout(function(){
-        $('li').removeClass('visible');
-    }, 300);
+if ('ontouchstart' in window) {
+    var click = 'touchstart';
+} else {
+    var click = 'click';
 }
 
-$('div.content').on(click, function(){
-    if( $('div.content').hasClass('flag') ){
-        closeMenu();
-    }
-});
+jQuery(document).ready(function ($) {
+    "use strict";
 
-$('li a').on(click, function(e){
-    e.preventDefault();
-    closeMenu();
+    /*	Reveal Menu */
+    $('div.button').on(click, function () {
+        if (!$('div.content').hasClass('inactive')) {
+
+            // Slide and scale content
+            $('div.content').addClass('inactive');
+            setTimeout(function () {
+                $('div.content').addClass('flag');
+            }, 100);
+
+            // Slide in menu links
+            var timer = 0;
+            $.each($('li'), function (i, v) {
+                timer = 40 * i;
+                setTimeout(function () {
+                    $(v).addClass('visible');
+                }, timer);
+            });
+        }
+    });
+
+    /*	Close Menu */
+    function closeMenu() {
+        // Slide and scale content
+        $('div.content').removeClass('inactive flag');
+
+        // Reset menu
+        setTimeout(function () {
+            $('li').removeClass('visible');
+        }, 300);
+    }
+
+    $('div.content').on(click, function () {
+        if ($('div.content').hasClass('flag')) {
+            closeMenu();
+        }
+    });
+
+    $('li a').on(click, function (e) {
+        e.preventDefault();
+        closeMenu();
+    });
 });
