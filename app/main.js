6392b9db5cfdee56cb4cc2c75ec5fed6ae879e6d
Matthias Kleine <matthias@klein0r.de>
2015-12-17 15:15:46 +0100

    Match height

--- app/main.js
+++ app/main.js
@@ -3,12 +3,14 @@ require.config({
     paths: {
         'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min',
         'angular-route': '//code.angularjs.org/1.4.6/angular-route.min',
-        'jquery': '//code.jquery.com/jquery-latest.min'
+        'jquery': '//code.jquery.com/jquery-latest.min',
+        'jquery-matchheight': '//cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.6.0/jquery.matchHeight-min'
     },
     shim: {
         'angular': {
             'exports': 'angular'
-        }
+        },
+        'jquery-matchheight': ['jquery']
     },
     callback: function () {
         'use strict';
